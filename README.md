ea8ab193bb1a4cc6bae49e9db716ec9b9297bfe7
Keegan Witt <keeganwitt@gmail.com>
2025-09-16 16:08:01 -0400

    Add example

---
+++ README.md
@@ -0,0 +1,5 @@
+# GMavenPlus Extensions Test
+
+Test by running
+1. `mvn clean package`
+2. `java -jar target/PROJECT.jar`
---
+++ src/main/groovy/ORG/PROJECT/main.groovy
@@ -0,0 +1,9 @@
+package ORG.PROJECT
+
+import java.time.LocalDate
+
+class Main {
+    static def main(def args) {
+        println LocalDate.now().toDate()
+    }
+}
